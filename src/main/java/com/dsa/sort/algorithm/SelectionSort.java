f476480a5acae986b291dd2d0ede9746c21c5671
Balaji Soundarrajan <balaji.pacific@gmail.com>
2019-06-02 06:14:22 -0400

    Updating with own logic

--- src/main/java/com/dsa/sort/algorithm/SelectionSort.java
+++ src/main/java/com/dsa/sort/algorithm/SelectionSort.java
@@ -8,36 +8,42 @@ package com.dsa.sort.algorithm;
  *
  */
 public class SelectionSort {
+	
+	private static int[] arrayValue = {156, 2, -15, 23, 893, 55, 2, -10, -456};
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		int[] arrayValue = {156, 2, -15, 23, 893, 55, 2, -10, -456};
-		
-		System.out.println("Before Sorting : ");
-		for(int x=0; x < arrayValue.length; x ++) {
-			System.out.print(arrayValue[x] + " ");
+		System.out.println("Before Sorting");		
+		for(int i=0;i<arrayValue.length;i++){
+			System.out.print(arrayValue[i] + " ");			
 		}
 		
-		for(int unsort=arrayValue.length -1; unsort > 0 ; unsort --) {			
-			int largest = 0;			
-			for(int i=1;i<=unsort;i++) {
-				if(arrayValue[i] > arrayValue[largest]) {
-					largest = i;
+		for(int unSortedPartation = arrayValue.length-1; unSortedPartation>=0; unSortedPartation--) {
+			for(int i=0;i<unSortedPartation;i++) {
+				if(arrayValue[i]>arrayValue[i+1]) {
+					swap(i,i+1);
 				}
 			}
-			int temp = arrayValue[largest];
-			arrayValue[largest] = arrayValue[unsort];
-			arrayValue[unsort] = temp;
-		}		
-		
-		System.out.println("\nAfter Sorting : ");
-		for(int x=0; x < arrayValue.length; x ++) {
-			System.out.print(arrayValue[x] + " ");
 		}
-
+		
+		System.out.println("\nAfter Sorting");		
+		for(int i=0;i<arrayValue.length;i++){
+			System.out.print(arrayValue[i] + " ");			
+		}
+	}
+	
+	/**
+	 * Swaping two number in the array
+	 * @param a
+	 * @param b
+	 */
+	public static void swap(int a, int b){
+		int temp = arrayValue[a];
+		arrayValue[a] = arrayValue[b];
+		arrayValue[b] = temp;
 	}
 
 }
