c408cfd4b1bea1f91d8a814e005b2092ea1c0ce6
Balaji Soundarrajan <balaji.soundarrajan@csra.com>
2019-08-07 14:45:57 -0400

    Two Dom Matrix Addition

--- src/main/java/com/dsa/array/probs/TwoDimensonalArray.java
+++ src/main/java/com/dsa/array/probs/TwoDimensonalArray.java
@@ -18,7 +18,9 @@ public class TwoDimensonalArray {
 	 */
 	public static void main(String[] args) {
 		
-		int[][] matrix = new int[10][10];		
+		int[][] matrixA = new int[10][10];		
+		int[][] matrixB = new int[10][10];
+		int[][] outputMatrix = new int[10][10];
 		int row = 0;
 		int column = 0;
 		
@@ -27,19 +29,49 @@ public class TwoDimensonalArray {
 		row = scanner.nextInt();
 		System.out.print("Enter size of the column : ");
 		column = scanner.nextInt();
-		System.out.println("Enter totally  " + row*column + " values");
+		System.out.println("Enter totally for Matrix A " + row*column + " values");
 		for(int i=0; i<row; i++) {
 			for (int j=0; j<column; j++) {
 				System.out.print("Enter value for " + i + "*" + j + " : " );
-				matrix[i][j] = scanner.nextInt();
+				matrixA[i][j] = scanner.nextInt();
 			}
 		}
 		
-		
-		System.out.println("Output : ");
+		System.out.println("Enter totally for Matrix B " + row*column + " values");
 		for(int i=0; i<row; i++) {
 			for (int j=0; j<column; j++) {
-				System.out.print(matrix[i][j] + " ");				
+				System.out.print("Enter value for " + i + "*" + j + " : " );
+				matrixB[i][j] = scanner.nextInt();
+			}
+		}
+		
+				
+		System.out.println("Matrix A: ");
+		for(int i=0; i<row; i++) {
+			for (int j=0; j<column; j++) {
+				System.out.print(matrixA[i][j] + " ");				
+			}
+			System.out.println("");
+		}
+		
+		System.out.println("Matrix B: ");
+		for(int i=0; i<row; i++) {
+			for (int j=0; j<column; j++) {
+				System.out.print(matrixB[i][j] + " ");				
+			}
+			System.out.println("");
+		}
+		
+		for(int i =0; i<row; i++) {
+			for(int j=0; j<column; j++) {
+				outputMatrix[i][j] = matrixA[i][j] + matrixB[i][j];				
+			}			
+		}
+		
+		System.out.println("Output Matrix: ");
+		for(int i=0; i<row; i++) {
+			for (int j=0; j<column; j++) {
+				System.out.print(outputMatrix[i][j] + " ");				
 			}
 			System.out.println("");
 		}
